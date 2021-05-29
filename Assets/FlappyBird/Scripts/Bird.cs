b33e284fa66663af17df55b8e70a7251ef0df818
Jekaaaa <85004140+Jekaaaa@users.noreply.github.com>
2021-05-29 10:55:58 +0700

    initial commit

---
+++ Assets/FlappyBird/Scripts/Bird.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public void Flap()
+    {
+
+    }
+
+    public void FallDown()
+    {
+
+    }
+
+    public void Dead()
+    {
+
+    }
+}
---
+++ Assets/FlappyBird/Scripts/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+   public void OnPassingPipe()
+   {
+
+   }
+
+    public void AddScore()
+    {
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+}
